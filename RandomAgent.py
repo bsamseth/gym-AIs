8364268ac422b2d5998762d71de8ccfa9f077893
Bendik Samseth <b.samseth@gmail.com>
2018-03-16 22:58:03 +0100

    Add RandomAgent - baseline for worst agent.
    Should be compatible with most environments.

---
+++ RandomAgent.py
@@ -0,0 +1,41 @@
+from commons.GymRunner import GymRunner
+from commons.QLearningAgent import QLearningAgent
+
+
+class RandomAgent(QLearningAgent):
+    """
+    The dumbest agent there is. Just produces a random move.
+    """
+
+    def __init__(self, action_space):
+        super().__init__(None)
+        self.is_trained = True
+
+        class RandomModel(object):
+            def predict(self, *args, **kwargs):
+                return [action_space.sample()]
+
+            def fit(self, *args, **kwargs):
+                pass
+
+            def load_weights(self, *args, **kwargs):
+                pass
+
+            def save_weights(self, *args, **kwargs):
+                pass
+
+        self.model = RandomModel()
+
+    def build_model(self):  # Should override abstract method.
+        pass
+
+
+if __name__ == "__main__":
+    env_id = 'CartPole-v0'
+    runner = GymRunner(env_id)
+    agent = RandomAgent(runner.env.action_space)
+
+    runner.run(agent, 1, render=True)
+
+    agent.close()
+    runner.close()
