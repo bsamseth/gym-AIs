289a2b6c7c4262da8a151f43df253bfe32858997
Bendik Samseth <b.samseth@gmail.com>
2018-03-16 15:34:26 +0100

    Add abstract base class for a Q-learning agent.

---
+++ commons/QLearningAgent.py
@@ -0,0 +1,122 @@
+import abc
+from collections import deque
+import numpy as np
+import random
+import os
+
+
+class QLearningAgent(object):
+    """
+    Abstract base class for a Q-learning agent.
+
+    This class provided the expected interface between classes
+    such as GymRunner, and the underlying model.
+
+    Subclasses may only need to supply an implementation of the
+    build_model() method, defining a specific model. It may also be of
+    interest to tune some of the hyper-parameters.
+    """
+
+    def __init__(self, action_space_size, model_store_file=None):
+        """
+        Initialize the agent.
+
+        If model_store_file is provided and it is a valid file, weights
+        will be loaded from this file, and the agent will be marked as trained.
+
+        If model_store_file is provided, regardless of its prior existence,
+        the model will be saved to this filename when the agent is closed.
+
+        :param action_space_size: number of actions that can be made in a state
+        :param model_store_file: file used to save model weights in
+        """
+        self.action_space_size = action_space_size
+        self.model_store_file = model_store_file
+        self.is_trained = False
+
+        self.gamma = 0.90  # Discount rate for future rewards.
+        self.epsilon = 1  # Probability of choosing random actions in training.
+        self.epsilon_decay = 0.995  # Decay rate applied per training batch.
+        self.epsilon_min = 0.1  # Minimum value epsilon may decay to.
+        self.batch_size = 30  # Size of training batches sampled when learning.
+
+        self.memory = deque(maxlen=2000)  # Container for observed simulations.
+
+        self.model = self.build_model()
+
+        # Initialize model weights if a valid file has been provided.
+        if self.model_store_file and os.path.isfile(self.model_store_file):
+            self.model.load_weights(self.model_store_file)
+            self.is_trained = True
+
+    @abc.abstractmethod
+    def build_model(self):
+        """
+        :return: A Keras-type model.
+        """
+        pass
+
+    def select_action(self, state, training=False):
+        """
+        Produce an action based on the learned model.
+
+        If training is true, we might chose a random action.
+        This is done with a probability of self.epsilon.
+
+        :param state: the state to produce an action from
+        :param training: true if we should include randomness
+        :return: An action to take, as a non-negative integer.
+        """
+        if training and random.random() < self.epsilon:
+            return random.randrange(self.action_space_size)
+        return np.argmax(self.model.predict(state)[0])
+
+    def record(self, state, action, reward, next_state, done):
+        """
+        Save the given simulation result in memory for later training.
+
+        :param state: the state in which we produced
+        :param action: this action,
+        :param reward: which gave this reward
+        :param next_state: and produced this next state
+        :param done: which meant the simulation was done (true/false)
+        """
+        self.memory.append((state, action, reward, next_state, done))
+
+    def learn(self):
+        """
+        Sample a random subset of the recorded simulations, and train the
+        model to better fit its response accordingly.
+        """
+        batch_size = min(self.batch_size, len(self.memory))
+
+        batch = random.sample(self.memory, batch_size)
+
+        for state, action, reward, next_state, done in batch:
+
+            # The target Q-value, given by Bellman's equation:
+            # Q(S, A) = R + discount * max( Q(S', A') for A' in descendant S' of S )
+            if done:
+                target = reward
+            else:
+                target = reward + \
+                         self.gamma * np.max(self.model.predict(next_state)[0])
+
+            # What model produces now:
+            target_response = self.model.predict(state)
+
+            # Modify value for action with the Bellman target:
+            target_response[0][action] = target
+
+            # Fit model to produce target_response when in state:
+            self.model.fit(state, target_response, epochs=1, verbose=0)
+
+        # Decay epsilon.
+        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+
+    def close(self):
+        """
+        If self.model_store_file is set, save the model weights.
+        """
+        if self.model_store_file:
+            self.model.save_weights(self.model_store_file, overwrite=True)
